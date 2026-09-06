f01f18f86ab509608713538796a57e1369b64e52
kayden <altacc4567@gmail.com>
2026-09-06 09:30:22 -0700

    Delete hw1.java

--- hw1.java
+++
@@ -1,9 +0,0 @@
-class hw1{
-    public static void main(String[] args){
-        String s = "Dominic";
-
-        for (Character c : s.toCharArray()){
-            System.out.println((int)c);
-        }
-    }
-}
